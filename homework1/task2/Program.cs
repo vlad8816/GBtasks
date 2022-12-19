4e1545b1878a5de06a97793fbb9c7c033204e311
Vlad <email@gmail.com>
2022-12-19 19:27:22 +0500

    Task2 done

--- homework1/task2/Program.cs
+++ homework1/task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+using static System.Console;
+Clear();
+WriteLine("Введите первое число ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число ");
+int b = int.Parse(ReadLine()!);
+int max = 0;
+if (a>b)
+{
+
+    max = a;
+    
+}
+else max = b;
+WriteLine($"max = {max}");
