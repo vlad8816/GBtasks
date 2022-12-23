ab24a81aaf6657ea2b82e0eed2dc3df27d4f2383
Vlad <email@gmail.com>
2022-12-23 21:53:04 +0500

    Task18 done with switch method

--- task18/Program.cs
+++ task18/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число 1-7");
+int number = Convert.ToInt32(ReadLine()!);
+
+switch(number)
+{
+    case 1:
+    {
+    WriteLine("Понедельник");
+    break;
+    }
+    case 2:
+    {
+        WriteLine("Вторник");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("Среда");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("Четверг");
+        break;
+    }
+    case 5:
+    {
+        WriteLine("Пятница");
+        break;
+    }
+    case 6:
+    {
+        WriteLine("Суббота");
+        break;
+    }
+    case 7:
+    {
+        WriteLine("Воскресенье");
+        break;
+    }
+    default:
+    {
+        WriteLine("Нет такого дня");
+        break;
+    }
+}
