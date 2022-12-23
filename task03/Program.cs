5f9e43513c477826bc7656004268dee14a0df8d0
Vlad <email@gmail.com>
2022-12-23 22:13:29 +0500

    Renamed

---
+++ task03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+using static System.Console;
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int start = -number;
+while (start < number)
+{
+    Write($" {start},");
+    start +=1;
+}
+WriteLine($" {start}");
