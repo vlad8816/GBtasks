2ea48efd00f4a71dc8ccff6104ac9114869a9cec
Vlad <email@gmail.com>
2022-12-24 00:26:47 +0500

    Task24 done

--- task24/Program.cs
+++ task24/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+using static System.Console;
+Clear();
+
+Write("Введите число от 1 до числа: ");
+bool isParse = int.TryParse(ReadLine(), out int N);
+if(!isParse)
+{
+    WriteLine("Не введено число");
+    return;
+}
+int i = 1;
+while(i <= N)
+{
+    WriteLine($"Квадрат числа {i} - {Math.Pow(i,2)}");
+    i+=1;
+}
