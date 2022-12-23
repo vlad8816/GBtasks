71dd5d29aeb47cfe3e29e5fe15f251a11d88d061
Vlad <email@gmail.com>
2022-12-23 17:17:05 +0500

    Task10 done

--- homework2/task10/Program.cs
+++ homework2/task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(ReadLine());
+int number_two = number % 100;
+int number_three = number_two/10;
+WriteLine(number_three);
