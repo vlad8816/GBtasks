35ffc9c611d1d298b7785ec668e37d69e7d4dbb6
Vlad <email@gmail.com>
2022-12-23 15:43:30 +0500

    Task12.1 done

--- task12.1/Program.cs
+++ task12.1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿////Напишите программу, которая будет принимать на вход два числа и выводить,только знак '>','<' или '=' между ними
+using static System.Console;
+Clear();
+
+WriteLine("Введите первое число ");
+int first_number = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число ");
+int second_number = Convert.ToInt32(ReadLine());
+WriteLine(first_number==second_number? "=":first_number>second_number?">":"<");// если первое число равно второму числу,то выводим '=',а иначе (:) если первое число больше второго числа выводим (истина)">" (ложь)"<"
