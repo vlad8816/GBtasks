476a2d25fadf894c00f5c812e7a8413504f70465
Vlad <email@gmail.com>
2022-12-24 00:46:58 +0500

    Task23 done

--- homework3/task23/Program.cs
+++ homework3/task23/Program.cs
@@ -2,3 +2,17 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+using static System.Console;
+Clear();
+
+Write("Введите число от 1 до ");
+bool isParse= int.TryParse(ReadLine(),out int n);
+if(!isParse)
+{
+    WriteLine("Не введено число");
+    return;
+}
+for (int i=1;i <= n; i+=1)
+{
+    WriteLine($"Куб числа {i} равен {Math.Pow(i,3)}");
+}
