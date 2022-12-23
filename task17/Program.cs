ac13667aa0ed84f87d51edaed4e576611619017b
Vlad <email@gmail.com>
2022-12-23 21:53:34 +0500

    Task17 done with switch method

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+using static System.Console;
+Clear();
+
+Write("Введите номер четверти:");
+int i = int.Parse(ReadLine()!);
+
+ switch(i)
+ {
+    case 1:
+    {
+        WriteLine("x > 0, y > 0");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("x < 0, y > 0");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("x < 0, y < 0");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("x > 0, y <0");
+        break;
+    }
+    default:
+    {
+        WriteLine("Ошибка");
+        break;
+    }
+ }
+// if(i < 1 || i > 4)
+// {
+//     WriteLine("Ошибка");
+//     return;
+
+// }
+// if (i == 1)
+// {
+//     WriteLine("x > 0, y > 0");
+// }
+// if (i == 2)
+// {
+//     WriteLine("x < 0, y > 0");
+// }
+// if (i == 3)
+// {
+//     WriteLine("x < 0, y < 0");
+// }
+// if (i == 4)
+// {
+//     WriteLine("x > 0, y <0");
+// }
