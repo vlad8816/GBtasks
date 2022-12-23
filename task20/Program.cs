9db9fabe0eebb821d8a2ab7a1b985ca1de59133e
Vlad <email@gmail.com>
2022-12-23 22:10:39 +0500

    Task20 done

--- task20/Program.cs
+++ task20/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите x");
+int x = Convert.ToInt32(ReadLine()!);
+WriteLine("Введите y");
+int y = Convert.ToInt32(ReadLine()!);
+if(x > 0 && y > 0)
+{
+    WriteLine("1 четверть");
+}
+if(x < 0 && y < 0)
+{
+    WriteLine("3 четверть");
+}
+if(x > 0 && y < 0)
+{
+    WriteLine("4 четверть");
+}
+if(x < 0 && y > 0)
+{
+    WriteLine("2 четверть");
+}
+if(x == 0 && y == 0)
+{
+    WriteLine("Начало координат");
+}
+
