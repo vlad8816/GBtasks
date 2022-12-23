021dfada4ef2ecdb287426da0214dfdf01e68a88
Vlad <email@gmail.com>
2022-12-23 16:36:19 +0500

    Task16 done with Ternar operator

--- task16/Program.cs
+++ task16/Program.cs
@@ -3,7 +3,9 @@
 using static System.Console;
 Clear();
 
-WriteLine("Введите первое число ")
-int number_one = Convert.ToInt32(ReadLIne());
+WriteLine("Введите первое число ");
+int number_one = Convert.ToInt32(ReadLine());
 WriteLine("Введите второе число ");
-int number_two = Convert/ToIn32(ReadLine());
+int number_two = Convert.ToInt32(ReadLine());
+WriteLine(number_one == number_two*number_two? "Первое число это квадрат второго":"Первое число не является квадратом второго");
+WriteLine(number_two == number_one*number_one? "Второе число это квадрат первого":"Второе число не является квадратом первого");
