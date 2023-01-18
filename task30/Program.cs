f0d04265ac22b04945e155341127c96abccead2f
Vlad <email@gmail.com>
2023-01-18 17:11:50 +0500

    Task30 done

--- task30/Program.cs
+++ task30/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+using static System.Console;
+Clear();
+
+WriteLine("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int multi = GetMulti(number);
+WriteLine($"Multiplication 1..N = {multi} ");
+
+
+int GetMulti (int A)
+{
+    int result = 1;
+    while(A > 0)
+    {
+       result*=A;
+        A--;
+    }
+    return result;
+}
