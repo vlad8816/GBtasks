b5ee7659265cbd4858839ba129b6c5be616c3f0b
Vlad <email@gmail.com>
2023-01-19 01:19:03 +0500

    Task29 done

--- homework4/task29/Program.cs
+++ homework4/task29/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, реализующую метод, который формирует массив случайных целых чисел из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19,6, 1, 33 -> [1, 2, 5, 7, 19,6, 1, 33 ]
+
+using static System.Console;
+Clear();
+
+int[] arr = GetArray(8);
+
+PrintArray(arr);
+
+int [] GetArray (int size)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,99);
+    }
+    return array;
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{arr[i]} ");
+    }
+}
