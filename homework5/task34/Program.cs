1851a7e3421a11ee3abe4a31c6632c4e5aea62bd
Vlad <email@gmail.com>
2023-01-23 17:45:16 +0500

    Task34 done

--- homework5/task34/Program.cs
+++ homework5/task34/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Напишите программу реализующую методы формирования массива, 
+// заполненного случайными положительными трёхзначными числами, и подсчета 
+// количества чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
+Clear();
+
+int[] arr = GetArray(5, 100, 1000);
+WriteLine("[{0}]", string.Join(", ", arr));
+int count1 = CountArray(arr);
+WriteLine($"Количество четных элементов в массиве = {count1}");
+
+int[] GetArray(int size, int start,int finish)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for(int i =0; i < size; i++)
+    {
+        array[i] = rnd.Next(start, finish);
+    } 
+    return array;
+}
+
+int CountArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2== 0) 
+        {
+            count +=1;
+        }
+    }
+    return count;
+}
