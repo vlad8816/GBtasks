b2e838770c07f5c8942f84ed98792cc1969768d2
Vlad <email@gmail.com>
2023-01-23 16:50:17 +0500

    task32 done

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//  [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+using static System.Console;
+Clear();
+
+int[] array = {-1, -2, 0, 2, 1, 3, 4};
+
+ReverseArray(array);
+PrintArray(array);
+
+
+
+
+void ReverseArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        inArray[i] *= -1;
+    }
+}
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
