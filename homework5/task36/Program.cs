2aaee82a813273776f33dcd3fc8050c081bd870a
Vlad <email@gmail.com>
2023-01-23 17:46:01 +0500

    Task36 done

--- homework5/task36/Program.cs
+++ homework5/task36/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 36: Напишите программу реализующую методы формирования массива, 
+заполненного случайными числами и подсчета суммы элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+using static System.Console;
+Clear();
+
+int[] arr = GetArray(4, -10, 10);
+WriteLine("[{0}]", string.Join(", ", arr));
+
+int result = count(arr);
+WriteLine(result);
+
+int[] GetArray(int size, int start, int finish)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(start, finish);
+    }
+    return array;
+}
+
+int count(int[] array)
+{
+    int count = 0;
+    for(int i = 1; i < array.Length; i += 2)
+    {
+        count += array[i]; 
+    }
+    return count;
+}
