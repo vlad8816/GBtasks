7c284ba1772a60be6055b7a9a5499f487719850a
Vlad <email@gmail.com>
2023-01-23 16:50:30 +0500

    task33 done

--- task33/Program.cs
+++ task33/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+
+// 3; массив [6, 7, 19, 345, 3] -> да
+using static System.Console;
+Clear();
+
+Write("ВВедите число: ");
+int.TryParse(ReadLine(), out int number);
+
+int [] array = {-1, -2, 0, 2, 1, 3, 4 };
+
+// bool result = ArrayLookUp(array, number);
+WriteLine(ArrayLookUp(array, number)? "да":"нет");
+
+bool ArrayLookUp(int[] inArray, int num)
+{
+    foreach (var i in inArray)
+    {
+        if (num == i) return true;
+    }
+    return false;
+}
