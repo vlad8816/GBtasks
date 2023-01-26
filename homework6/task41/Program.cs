cdb800c9d950d5112202aea37bb25cac8b5e3873
Vlad <email@gmail.com>
2023-01-26 21:09:42 +0500

    Task41 done

--- homework6/task41/Program.cs
+++ homework6/task41/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите через пробел, произвольное количество чисел");
+string[] UserNumbers = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine($"Пользователем введено {CountNumbers(UserNumbers)} числа больше нуля");
+
+int CountNumbers(string[] st)
+{ 
+    int count = 0;
+    for (int i = 0; i < st.Length; i++)
+    {
+        if(Convert.ToInt32(st[i]) > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
